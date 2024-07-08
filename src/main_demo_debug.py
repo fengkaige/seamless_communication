badbff01a4877bd91259e595642d8237fc87e668
fengkaige <kaige.feng@lynxi.com>
2024-07-08 09:51:36 +0800

    [Feature]Opt main_demo_debug.py code.

--- src/main_demo_debug.py
+++ src/main_demo_debug.py
@@ -12,6 +12,7 @@ import torchaudio
 import torch
 
 from collections import defaultdict
+
 # from IPython.display import Audio, display
 from pathlib import Path
 from pydub import AudioSegment
@@ -23,7 +24,8 @@ from seamless_communication.streaming.dataloaders.s2tt import SileroVADSilenceRe
 import math
 from simuleval.data.segments import SpeechSegment, EmptySegment
 from seamless_communication.streaming.agents.seamless_streaming_s2st import (
-    SeamlessStreamingS2STVADAgent,)
+    SeamlessStreamingS2STVADAgent,
+)
 
 from simuleval.utils.arguments import cli_argument_list
 from simuleval import options
@@ -39,257 +41,21 @@ from config import LyngorBuildFlags, ModelSaveWeightFlags
 # sample_rate : 是什么含义？
 SAMPLE_RATE = 16000
 
-class AudioFrontEnd:
-    def __init__(self, wav_file, segment_size) -> None:
-        self.samples, self.sample_rate = soundfile.read(wav_file)
-        # print(self.sample_rate, SAMPLE_RATE)
-        print('sample_rate', self.sample_rate)
-        assert self.sample_rate == SAMPLE_RATE
-        # print(len(self.samples), self.samples[:100])
-        self.samples = self.samples  # .tolist()
-        self.segment_size = segment_size
-        self.step = 0
-        print('len samples', len(self.samples))
-
-    def send_segment(self):
-        """
-        This is the front-end logic in simuleval instance.py
-        """
-
-        num_samples = math.ceil(self.segment_size / 1000 * self.sample_rate)
-        print('num_samples of this segment', num_samples)
-
-        if self.step < len(self.samples):
-            if self.step + num_samples >= len(self.samples):
-                samples = self.samples[self.step:]
-                is_finished = True
-            else:
-                samples = self.samples[self.step: self.step + num_samples]
-                is_finished = False
-            self.step = min(self.step + num_samples, len(self.samples))
-
-            segment = SpeechSegment(
-                content=samples,
-                sample_rate=self.sample_rate,
-                finished=is_finished,
-            )
-        else:
-            # Finish reading this audio
-            segment = EmptySegment(
-                finished=True,
-            )
-        return segment
-
-
-class OutputSegments:
-    def __init__(self, segments: Union[List[Segment], Segment]):
-        if isinstance(segments, Segment):
-            segments = [segments]
-        self.segments: List[Segment] = [s for s in segments]
-
-    @property
-    def is_empty(self):
-        return all(segment.is_empty for segment in self.segments)
-
-    @property
-    def finished(self):
-        return all(segment.finished for segment in self.segments)
-
-
-def get_audiosegment(samples, sr):
-    b = io.BytesIO()
-    soundfile.write(b, samples, samplerate=sr, format="wav")
-    b.seek(0)
-    return AudioSegment.from_file(b)
-
-
-def reset_states(system, states):
-    if isinstance(system, TreeAgentPipeline):
-        states_iter = states.values()
-    else:
-        states_iter = states
-    for state in states_iter:
-        state.reset()
-
-
-def get_states_root(system, states) -> AgentStates:
-    if isinstance(system, TreeAgentPipeline):
-        # self.states is a dict
-        return states[system.source_module]
-    else:
-        # self.states is a list
-        return system.states[0]
-
-
-def plot_s2st(source_file, target_samples, target_fs, intervals, delays, prediction_lists):
-    mpl.rcParams["axes.spines.left"] = False
-    mpl.rcParams["axes.spines.right"] = False
-    mpl.rcParams["axes.spines.top"] = False
-    mpl.rcParams["axes.spines.bottom"] = False
-
-    source_samples, source_fs = soundfile.read(source_file)
-
-    _, axes = plt.subplots(5, sharex=True, figsize=(25, 5))
-    for ax in axes:
-        ax.set_yticks([])
-
-    axes[0].plot(
-        numpy.linspace(0, len(source_samples) /
-                       source_fs, len(source_samples)),
-        source_samples,
-    )
-
-    axes[1].plot(
-        numpy.linspace(0, len(target_samples) /
-                       target_fs, len(target_samples)),
-        target_samples,
-    )
-
-    start = 0
-    for seg_index in range(len(intervals)):
-        start, duration = intervals[seg_index]
-        offset = delays["s2st"][seg_index]
-
-        samples = target_samples[
-            int((start) / 1000 * target_fs): int(
-                (start + duration) / 1000 * target_fs
-            )
-        ]
-
-        # Uncomment this if you want to see the segments without speech playback delay
-        axes[2].plot(
-            offset / 1000 +
-            numpy.linspace(0, len(samples) / target_fs, len(samples)),
-            -seg_index * 0.05 + numpy.array(samples),
-        )
-        axes[4].plot(
-            start / 1000 +
-            numpy.linspace(0, len(samples) / target_fs, len(samples)),
-            numpy.array(samples),
-        )
-
-    from pydub import AudioSegment
-    print("Output translation (without input)")
-    # display(Audio(target_samples, rate=target_fs))
-    print("Output translation (overlay with input)")
-    source_seg = get_audiosegment(
-        source_samples, source_fs) + AudioSegment.silent(duration=3000)
-    target_seg = get_audiosegment(target_samples, target_fs)
-    output_seg = source_seg.overlay(target_seg)
-    # display(output_seg)
-
-    delay_token = defaultdict(list)
-    d = delays["s2tt"][0]
-    for token, delay in zip(prediction_lists["s2tt"], delays["s2tt"]):
-        if delay != d:
-            d = delay
-        delay_token[d].append(token)
-    for key, value in delay_token.items():
-        axes[3].text(
-            key / 1000, 0.2, " ".join(value), rotation=40
-        )
-
-
-def build_streaming_system(model_configs, agent_class):
-    parser = options.general_parser()
-    parser.add_argument(
-        "-f", "--f", help="a dummy argument to fool ipython", default="1")
-
-    agent_class.add_args(parser)
-    args, _ = parser.parse_known_args(cli_argument_list(model_configs))
-    system = agent_class.from_args(args)
-    return system
-
-
-def run_streaming_inference(system, audio_frontend, system_states, tgt_lang):
-    # NOTE: Here for visualization, we calculate delays offset from audio
-    # *BEFORE* VAD segmentation.
-    # In contrast for SimulEval evaluation, we assume audios are pre-segmented,
-    # and Average Lagging, End Offset metrics are based on those pre-segmented audios.
-    # Thus, delays here are *NOT* comparable to SimulEval per-segment delays
-    delays = {"s2st": [], "s2tt": []}
-    prediction_lists = {"s2st": [], "s2tt": []}
-    speech_durations = []
-    curr_delay = 0
-    target_sample_rate = None
-
-    while_count = 0
-    while True:
-        print("while_count:", while_count)
-        while_count += 1
-
-        input_segment = audio_frontend.send_segment()
-        input_segment.tgt_lang = tgt_lang
-        curr_delay += len(input_segment.content) / SAMPLE_RATE * 1000
-        if input_segment.finished:
-            # a hack, we expect a real stream to end with silence
-            get_states_root(system, system_states).source_finished = True
-        # Translation happens here
-
-        # import pdb;pdb.set_trace()
-
-        # output = system.pushpop(input_segment, system_states)
-
-        output_segments = OutputSegments(
-            system.pushpop(input_segment, system_states))
-        if not output_segments.is_empty:
-            for segment in output_segments.segments:
-                # NOTE: another difference from SimulEval evaluation -
-                # delays are accumulated per-token
-                if isinstance(segment, SpeechSegment):
-                    print("SpeechSegment")
-
-                    pred_duration = 1000 * \
-                        len(segment.content) / segment.sample_rate
-                    speech_durations.append(pred_duration)
-                    delays["s2st"].append(curr_delay)
-                    prediction_lists["s2st"].append(segment.content)
-                    target_sample_rate = segment.sample_rate
-                elif isinstance(segment, TextSegment):
-                    print("TextSegment")
-
-                    delays["s2tt"].append(curr_delay)
-                    prediction_lists["s2tt"].append(segment.content)
-                    print(curr_delay, segment.content)
-                elif isinstance(segment, EmptySegment):
-                    # do nothing
-                    print("EmptySegment")
-
-        if output_segments.finished:
-            print("End of VAD segment")
-            reset_states(system, system_states)
-        if input_segment.finished:
-            # an assumption of SimulEval agents -
-            # once source_finished=True, generate until output translation is finished
-            assert output_segments.finished
-            break
-    return delays, prediction_lists, speech_durations, target_sample_rate
-
-
-def get_s2st_delayed_targets(delays, target_sample_rate, prediction_lists, speech_durations):
-    # get calculate intervals + durations for s2st
-    intervals = []
-
-    start = prev_end = prediction_offset = delays["s2st"][0]
-    target_samples = [0.0] * int(target_sample_rate * prediction_offset / 1000)
-
-    for i, delay in enumerate(delays["s2st"]):
-        start = max(prev_end, delay)
-
-        if start > prev_end:
-            # Wait source speech, add discontinuity with silence
-            target_samples += [0.0] * int(
-                target_sample_rate * (start - prev_end) / 1000
-            )
-
-        target_samples += prediction_lists["s2st"][i]
-        duration = speech_durations[i]
-        prev_end = start + duration
-        intervals.append([start, duration])
-    return target_samples, intervals
-
+from main_demo import AudioFrontEnd, OutputSegments
+from main_demo import (
+    get_audiosegment,
+    reset_states,
+    get_states_root,
+    plot_s2st,
+    build_streaming_system,
+    run_streaming_inference,
+)
+from .main_demo import get_s2st_delayed_targets
 
 print("building system from dir")
+
+###### Flag
+###### >>>>>> >>>>>> >>>>>>
 
 # Set build flag by os env.
 lyngor_build_flag = LyngorBuildFlags()
@@ -303,6 +69,7 @@ save_weight_flag.save_offlineWav2VecBertEncoderAgent = "True"
 save_weight_flag.save_vocoderAgent = "False"
 save_weight_flag.init_os_env()
 
+###### <<<<<< <<<<<< <<<<<<
 
 agent_class = SeamlessStreamingS2STJointVADAgent
 tgt_lang = "spa"
@@ -341,8 +108,8 @@ system_states = system.build_states()
 # you can pass tgt_lang at inference time to change the output lang.
 # SeamlessStreaming supports 36 speech output languages, see https://github.com/facebookresearch/seamless_communication/blob/main/docs/m4t/README.md#supported-languages
 # in the Target column for `Sp` outputs.
-delays, prediction_lists, speech_durations, target_sample_rate = run_streaming_inference(
-    system, audio_frontend, system_states, tgt_lang
+delays, prediction_lists, speech_durations, target_sample_rate = (
+    run_streaming_inference(system, audio_frontend, system_states, tgt_lang)
 )
 
 
@@ -355,19 +122,24 @@ print(prediction_lists.keys())
 # print(speech_durations)
 # print(target_sample_rate)
 
-print('text outputs ', prediction_lists["s2tt"])
+print("text outputs ", prediction_lists["s2tt"])
 wave = prediction_lists["s2st"]
 for i in range(len(wave)):
-    print('wave', i)
-    print('length', len(wave[i]))
-    print('duration', len(wave[i])/target_sample_rate)
+    print("wave", i)
+    print("length", len(wave[i]))
+    print("duration", len(wave[i]) / target_sample_rate)
     plt.plot(wave[i])
-    plt.savefig("./output/wave"+str(i)+".png", dpi=300)
+    plt.savefig("./output/wave" + str(i) + ".png", dpi=300)
 
 wave_total = []
 for i in range(len(wave)):
     wave_total.extend(wave[i])
 print(len(wave_total))
 # save as wav
-soundfile.write("./output/reading.wav", wave_total,
-                target_sample_rate, format="WAV", subtype="PCM_16")
+soundfile.write(
+    "./output/reading.wav",
+    wave_total,
+    target_sample_rate,
+    format="WAV",
+    subtype="PCM_16",
+)
